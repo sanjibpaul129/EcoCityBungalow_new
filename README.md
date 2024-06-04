4bf70ff13f94a44fcaac6112af477213f8b27b8e
jaingroupsystem2 <system2.thejaingroup.com>
2024-06-04 18:03:11 +0530

    done

--- README.md
+++
@@ -1,1 +0,0 @@
-# EcoCityBungalow_new
---
+++ bungalowthankyou.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5XV9C7KV');</script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WHPNRVG');
+    </script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-P2GTXJR');
+    </script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager -->
+
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5MFQHBC3');
+    </script>
+    <!-- End Google Tag Manager -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link type="text/css" rel="stylesheet" href="assets/css" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Dream Eco City Bungalow</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="stylesheet" id="wp-block-library-css" href="assets/style.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="wpml-legacy-dropdown-0-css" href="assets/style(1).css" type="text/css" media="all" />
+    <link rel="stylesheet" id="wpml-tm-admin-bar-css" href="assets/admin-bar-style.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="parent-style-css" href="assets/style(2).css" type="text/css" media="all" />
+    <link rel="stylesheet" id="slick-css" href="assets/slick.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="mmenu-css" href="assets/mmenu.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="custom-select-css-css" href="assets/bootstrap-select.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="swiperjs-css-css" href="assets/swiper.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="aosjs-css-css" href="assets/aos.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="custom-google-fonts-css" href="assets/css(1)" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" id="wp-bootstrap-starter-style-css" href="assets/style(3).css" type="text/css" media="all" />
+    <link rel="stylesheet" id="easy-social-share-buttons-subscribe-css" href="assets/essb-subscribe.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="easy-social-share-buttons-css" href="assets/easy-social-share-buttons.min.css" type="text/css" media="all" />
+
+    <!--[if lt IE 9]>
+            <script type="text/javascript" src="https://properties.emaar.com/wp-content/themes/wp-bootstrap-starter/inc/assets/js/html5.js?ver=3.7.0"></script>
+        <![endif]-->
+    <link href="assets/pro-v4-shims.min.css" media="all" rel="stylesheet" />
+    <link href="assets/pro-v4-font-face.min.css" media="all" rel="stylesheet" />
+    <link href="assets/pro.min.css" media="all" rel="stylesheet" />
+    <link rel="stylesheet" id="gforms_reset_css-css" href="assets/formreset.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="gforms_formsmain_css-css" href="assets/formsmain.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="gforms_ready_class_css-css" href="assets/readyclass.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="gforms_browsers_css-css" href="assets/browsers.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" id="gforms_browsers_css-css" href="assets/lightbox.css" type="text/css" media="all" />
+
+    <link rel="stylesheet" href="sp-style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="assets/fonts/sp-fonts.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="font/fonts/flaticon.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="font/font/flaticon.css" type="text/css" media="all" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background-image: url('new1/img/glry6.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            backdrop-filter: blur(2px);
+            /* Adjust the pixel value to increase or decrease the blur effect */
+        }
+
+        .email_display {
+                color: #fff;
+                display: none;
+            }
+
+            .mobile_display {
+                color: #fff;
+                display: none;
+            }
+    </style>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NMZS5B9');
+    </script>
+    <!-- End Google Tag Manager -->
+</head>
+<body class="properties-template-default single single-properties postid-1009 group-blog mm-wrapper" data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XV9C7KV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHPNRVG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2GTXJR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMZS5B9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MFQHBC3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <div class="mm-page mm-slideout" id="mm-0">
+        <div id="page" class="site">
+            <div id="sp-header" class="">
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>-->
+                            <a class="navbar-brand" href="index.php"><img src="assets/logo.png" class="img-responsive" /></a>
+                        </div>
+                        <!--<div class="collapse navbar-collapse" id="myNavbar">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li class="active"><a href="index.php">Home</a></li>
+                                    <li><a href="#overview">Overview</a></li>
+                                    <li><a href="#location">Location</a></li>
+                                    <li><a href="#flat-tour">GALLERY</a></li>
+                                    <li><a href="#floor-plan">Floor Plans</a></li>
+                                    <li><a href="#why">Why Dream Eco City?</a></li>
+                                    <li><a href="#about">About us</a></li>
+                                </ul>
+                            </div>-->
+                    </div>
+                </nav>
+            </div>
+            <div id="content" class="site-content">
+                <div class="container-fluid" style="text-align: center; padding: 100px 15px;">
+                    <center><img src="tick.png" class="img-responsive" style="width: 90px; margin: 0 0 28px;"></center>
+                    <div class="row">
+                        <p id="email_data" class="email_display"></p>
+                        <p id="mobile_data" class="mobile_display"></p>
+                    </div>
+
+                    <h1 style=" color: white; text-transform: uppercase; font-weight: 900; font-size: 40px; margin: 15px 0 20px;">thank you !</h1>
+                    <p style="text-align: center; color: white; font-size: 22px;">Your submission is received and we will contact you soon.</p>
+                    <a href="http://dreamecocity.in" style=" background: #071c35; color: white; text-transform: capitalize; padding: 13px 20px; font-size: 15px; margin: 20px auto;  display: table;">back to home</a>
+                </div>
+                <footer id="colophon" class="site-footer navbar-light text-center text-md-left" role="contentinfo">
+                    <div class="footer-bottom">
+                        <div class="container text-center">
+                            <div class="menu-legal-menu-container">
+                                <ul id="menu-legal-menu" class="menu">
+                                    <li id="menu-item-808" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-808 hidden-xs"><i class="fa fa-phone-alt"></i>+91 8442 800 800 </li>
+                                    <li id="menu-item-808" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-808 visible-xs"><a href="tel:+918442800800"><i class="fa fa-phone-alt"></i>+91 8442 800 800 </a></li>
+                                    <li id="menu-item-809" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-809"> Jain Group © 2023, All Rights Reserved.
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <!--<a class="hidden-lg hidden-sm hidden-md" style="position: fixed; right: 0; top: 62%; z-index: 9999; width: 37px;" href="tel:+918442800800" target="_blank">
+<img src="call.png" class="img-responsive">
+</a>
+<a style="position: fixed; right: 0; top: 70%; z-index: 9999; width: 37px;" href="https://wa.me/919903322202?text=Interested%20in%20Dream%20Eco%20City" target="_blank"><img src="wh.png" class="img-responsive"></a>-->
+        <script src="js/lightbox.js"></script>
+        <script src="js/youtube.js"></script>
+        <script src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
+        <script>
+            window.onscroll = function() {
+                myFunction();
+            };
+
+            var header = document.getElementById("sp-header");
+            var sticky = header.offsetTop;
+
+            function myFunction() {
+                if (window.pageYOffset > sticky) {
+                    header.classList.add("sticky");
+                } else {
+                    header.classList.remove("sticky");
+                }
+            }
+        </script>
+        <script type="text/javascript">
+            var emailvariable = "";
+            var contactvariable = "";
+            jQuery(document).ready(function($) {
+                function getParameterByName(name) {
+                    name = name.replace(/[\[]/, "\[").replace(/[\]]/, "\]");
+                    var regex = new RegExp("[\?&]" + name + "=([^&#]*)"),
+                        results = regex.exec(location.search);
+                    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+                }
+                var emailvariable = '"' + getParameterByName('email').toString() + '"';
+                var contactvariable = '+91' + '"' + getParameterByName('contact').toString() + '"';
+                console.log(emailvariable);
+                console.log(contactvariable);
+                console.log("======================");
+                document.getElementById("email_data").textContent = emailvariable;
+                document.getElementById("mobile_data").textContent = contactvariable;
+            });
+        </script>
+</body>
+</html>
